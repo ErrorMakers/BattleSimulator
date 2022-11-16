0f3c7f7f7ac0b35557cfe9ed97e5b8a6dc6902d6
MarianoP26 <marianopierretti20@hotmail.com>
2022-11-16 15:05:32 +0100

    advanced

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Ultimate Battle IronBattle Simulator!");
         Game game = new Game();
         game.init();
+
+        System.out.println("Character initialization finished, enter any input to continue to setup");
+        scanner.nextLine();
+        game.setup();
+
     }
 }
