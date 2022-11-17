cbf66af671a4bd8782cd0efbdc5cb391a0da4765
Juan Sóto <juansheba97@gmail.com>
2022-11-17 11:36:50 +0100

    Fix input validation

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,9 +9,7 @@ public class Menu {
 
 
     public int letPlayerChooseGameMode() {
-        System.out.println("Choose a game mode... \n1-Create your Characters\n2-Create Random Characters\n3-Import from CSV");
-        //TODO: Check for correct input values
-        return scanner.nextInt();
+        return inputValidation("Choose a game mode... \n1-Create your Characters\n2-Create Random Characters\n3-Import from CSV", scanner);
     }
 
     public String createName() {
@@ -25,17 +24,13 @@ public class Menu {
             characterChoice = inputValidation("Choose between Warrior or Wizard...\n1-Warrior\n2-Wizard", scanner);
             if (characterChoice == 1) {
                 int hp = inputValidation("Set your hp: ", scanner);
-                int strength = inputValidation()
-                System.out.println("Set your stamina: ");
-                int stamina = scanner.nextInt();
+                int strength = inputValidation("Set your strength", scanner);
+                int stamina = inputValidation("Set your stamina: ", scanner);
                 return new int[]{characterChoice, hp, stamina, strength};
             } else if (characterChoice == 2) {
-                System.out.println("Set your hp: ");
-                int hp = scanner.nextInt();
-                System.out.println("Set your intelligence: ");
-                int intelligence = scanner.nextInt();
-                System.out.println("Set your mana: ");
-                int mana = scanner.nextInt();
+                int hp = inputValidation("Set your hp: ", scanner);
+                int intelligence = inputValidation("Set your intelligence: ", scanner);
+                int mana = inputValidation("Set your mana: ", scanner);
                 return new int[]{characterChoice, hp, mana, intelligence};
             } else {
                 System.out.println("Please, enter a valid character");
@@ -51,7 +46,7 @@ public class Menu {
         int counter = 1;
         for (String line : data) {
             attributes = line.split(",");
-            if (attributes[0].equals("warrior")) {
+            if (attributes[0].equals("Warrior")) {
                 System.out.println(counter + "- Type: " + attributes[0] + " | Name: " + attributes[1] + " | HP: " + attributes[2] + " | STA: " + attributes[3] + " | STR: " + attributes[4]);
                 counter++;
             }else {
@@ -74,7 +69,16 @@ public class Menu {
         this.isChoosingGameMode = isChoosingGameMode;
     }
 
-
+    public int inputValidation(String message, Scanner scanner){
+        System.out.println(message);
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("You didn't enter a Integer");
+            scanner.next();
+            return inputValidation(message, scanner);
+        }
+    }
 
 }
 
